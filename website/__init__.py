2df0f59a58146c5fc554d65e80823f034b50b9ad
Laihi Bahar Eddine <bel7@alu.ua.es>
2024-03-30 18:41:23 +0100

    Añadir la pagina home

--- website/__init__.py
+++ website/__init__.py
@@ -4,6 +4,9 @@ def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = '2<?,}p46#6A7SS7z5A1k%VE*&m)O|?'
 
+    app.static_folder = 'static'
+
+
     from .views import views
 
     app.register_blueprint(views, url_prefix='/')
