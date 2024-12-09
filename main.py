fadd5dcf4c6e9fc562ec21e2fdd317b1602e4de9
Laihi Bahar Eddine <bel7@alu.ua.es>
2024-12-09 13:24:14 +0100

    update

--- main.py
+++ main.py
@@ -3,5 +3,5 @@ from website import create_app
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(debug=True,port=2024)
+    app.run(debug=True,port=80)
     app.config["MAX_CONTENT_LENGTH"] = 50 * 1024 * 1024  # 50MB
